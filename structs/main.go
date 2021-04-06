fed426c486f994d32420cc9e892514988974d0ec
Clyne Paulo Fuentes <clippefuentes@gmail.com>
2021-04-05 23:24:43 -0600

    Add structs for go practice code

---
+++ structs/main.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+type person struct {
+	firstName string
+	lastName  string
+	contact contactinfo
+}
+
+type contactinfo struct {
+	email string
+	zipCode int
+}
+
+func main() {
+	// var clyne person
+	// clyne.firstName = "clyne"
+	// clyne.lastName = "fuentes"
+	clyne := person{
+		firstName: "clyne", 
+		lastName: "fuentes",
+		contact: contactinfo{
+			email: "clyne@email",
+			zipCode: 123,
+		},
+	}
+
+	clyne.updateName("clippe")
+	fmt.Println(clyne)
+	clyne.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
